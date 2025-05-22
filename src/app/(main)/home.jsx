8973487e3114d67acb0c2bc2aaeec694958bc56e
jaeden <jaedenthia22@gmail.com>
2025-05-23 01:02:03 +0800

    loading background using Asset and created a basic loading splash component

--- src/app/(main)/home.jsx
+++ src/app/(main)/home.jsx
@@ -2,8 +2,35 @@ import { StyleSheet, Text, View, ImageBackground } from 'react-native'
 import React from 'react'
 import Background from '../../assets/Backgrounds/BGHomeDay.png';
 import TackPrototype from '../../components/TackPrototype.jsx';
+import LoadingSplash from '../../components/LoadingSplash.jsx';
+import { Asset } from 'expo-asset';
 
 const home = () => {
+  let [isLoaded, setIsLoaded] = React.useState(false);
+
+  let cacheResources = async() => {
+    const assetPromise = Asset.fromModule(Background).downloadAsync();
+    const delayPromise = new Promise((resolve) => setTimeout(resolve, 1000));
+
+    await Promise.all([assetPromise, delayPromise]);
+    setIsLoaded(true);
+  }
+
+  React.useEffect(() => {
+    const loadResources = async() => {
+      await cacheResources();
+      setIsLoaded(true);
+    };
+
+    loadResources();
+  }, [])
+
+  if (!isLoaded) {
+    return (
+      <LoadingSplash />
+    );
+  }
+  
   return ( 
     <ImageBackground 
         source = {Background}
