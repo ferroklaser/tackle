66d7d4c709a4e28f134422f64593957ddfdc0307
Win Paing Oo <winpaing000@gmail.com>
2025-05-26 21:58:01 +0800

    Refactor: Change Auth Button

---
+++ src/components/AuthComponents/AuthButton.jsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View , TouchableOpacity} from 'react-native'
+import React from 'react'
+
+const AuthButton = ({onPress, style, ...props}) => {
+  return (
+    <TouchableOpacity style={ [styles.button, style] } onPress={onPress} activeOpacity={.5} {...props}/>
+  )
+}
+
+export default AuthButton
+
+const styles = StyleSheet.create({
+    button: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+
+        padding: 15,
+        backgroundColor: 'white',
+        width: '40%',
+        borderRadius: 10,
+        alignItems: 'center',
+    }
+})
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -19,7 +19,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+// export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 // initializeAuth(FIREBASE_APP, { 
 //    persistence: getReactNativePersistence(ReactNativeAsyncStorage)});
 
