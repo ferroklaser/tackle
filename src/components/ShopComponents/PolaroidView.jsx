04ea07a46999b9282077df5cbf414512aa419437
Win Paing Oo <winpaing000@gmail.com>
2025-06-14 21:46:45 +0800

    feat(shop): add polaroid details

--- src/components/ShopComponents/PolaroidView.jsx
+++ src/components/ShopComponents/PolaroidView.jsx
@@ -4,8 +4,13 @@ import React from 'react'
 const PolaroidView = () => {
   return (
     <View style={styles.container}>
-      <Text>PolaroidView</Text>
-    </View>
+      <View style={styles.dummy}></View>
+          <View style={styles.details}> 
+              <Text style={styles.text}>Name</Text>
+              <Text style={styles.text}>Type</Text>
+              <Text style={styles.text}>Price</Text>
+          </View>
+      </View>
   )
 }
 
@@ -13,11 +18,31 @@ export default PolaroidView
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "yellow",
-        borderWidth: 2,
+        backgroundColor: "#FFE98A",
+        borderWidth: 1,
         borderColor: 'black',
-        height: '90%',
-        width: '30%',
-        margin: 20
+        height: 170,
+        width: 130,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 20,
+
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+        elevation: 4,
+    },
+    dummy: {
+        backgroundColor: 'white',
+        width: 100,
+        height: 100,
+    },
+    details: {
+        marginTop: 10,
+    },
+    text: {
+        fontSize: 12,
+        textAlign: 'left'
     }
 })
