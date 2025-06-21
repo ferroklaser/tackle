708b45394ec1a73eddc3fe2711e8369ff510c43e
Win Paing Oo <winpaing000@gmail.com>
2025-06-21 12:30:26 +0800

    feat(shop): items shown in shop

--- src/app/(protected)/(main)/(tabs)/store.jsx
+++ src/app/(protected)/(main)/(tabs)/store.jsx
@@ -1,17 +1,31 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import PolaroidView from '../../../../components/ShopComponents/PolaroidView'
-
+import { generateUserShop } from '../../../../utilities/generateUserShop.js'
+import { useAuth } from '../../../../contexts/AuthContext.jsx'
+import { useState, useEffect } from 'react'
 
 const store = () => {
+  const { user } = useAuth();
+  const [shop, setShop] = useState();
+  
+  useEffect(() => {
+    const fetchShop = async () => {
+      try {
+        const newShop = await generateUserShop(user);
+        setShop(newShop);
+      } catch (error) {
+        console.log("Error fetching shop: ", error)
+      }
+    }
+    fetchShop();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <PolaroidView/>
-      <PolaroidView/>
-      <PolaroidView/>
-      <PolaroidView/>
-      <PolaroidView/>
-      <PolaroidView/>
+      {shop && shop.map(item => (
+        <PolaroidView key={item.itemID} item={item}/>
+      ))}
     </View>
   )
 }
