72cd3b73fbb41344daee0148600e8102039277ed
jaeden <jaedenthia22@gmail.com>
2025-06-28 12:41:20 +0800

    feat: tack icons added into assets

---
+++ src/assets/TackIcons/Eyes/index.js
@@ -0,0 +1,17 @@
+import Excited from './Eyes_Excited.png';
+import Side_Eye from './Eyes_SideEye.png';
+import Angry from './Eyes_Angry.png';
+import Glasses from './Eyes_Glasses.png';
+import Hearts from './Eyes_Hearts.png';
+import Shocked from './Eyes_Shocked.png';
+import Tired from './Eyes_Tired.png';
+
+export default {
+    Excited,
+    Side_Eye,
+    Angry,
+    Glasses,
+    Hearts,
+    Shocked,
+    Tired
+};
---
+++ src/assets/TackIcons/TackBase/index.js
@@ -0,0 +1,13 @@
+import Blue from './TackBlue.png';
+import Yellow from './TackYellow.png';
+import Green from './TackGreen.png';
+import Pink from './TackPink.png';
+import Purple from './TackPurple.png';
+
+export default {
+    Blue,
+    Yellow,
+    Green,
+    Pink,
+    Purple,
+};
