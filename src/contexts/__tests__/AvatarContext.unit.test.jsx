cd6b676ffa59c7a15ffce0f4ad5e664ec79c76d3
Win Paing Oo <winpaing000@gmail.com>
2025-07-01 23:05:10 +0800

    feat: add unit test for avatar context

---
+++ src/contexts/__tests__/AvatarContext.unit.test.jsx
@@ -0,0 +1,99 @@
+import { render, renderHook, waitFor, act } from "@testing-library/react-native";
+import { AvatarProvider, useAvatar } from "../AvatarContext";
+import { updateDoc, doc } from 'firebase/firestore'
+import * as AssetModule from 'expo-asset'
+
+
+const wrapper = ({children}) => <AvatarProvider>{children}</AvatarProvider>
+
+jest.mock('expo-asset');
+
+AssetModule.Asset.fromModule = () => ({
+    downloadAsync: jest.fn().mockResolvedValue(true)
+})
+
+jest.mock('../../firebaseConfig', () => ({
+  FIREBASE_AUTH: {
+    currentUser: {
+      uid: 'test-uid',
+    },
+  },
+  FIREBASE_DATABASE: {}, // mock this if needed
+}));
+
+
+jest.mock('../AuthContext', () => ({
+    useAuth: () => ({
+        user: {uid: 'test-uid'},
+    })
+}))
+
+jest.mock('expo-asset', () => ({
+  Asset: {
+    fromModule: () => ({
+      downloadAsync: jest.fn().mockResolvedValue(true),
+    }),
+  },
+}));
+
+jest.mock('firebase/firestore', () => ({
+  ...jest.requireActual('firebase/firestore'),
+  onSnapshot: jest.fn((ref, callback) => {
+    callback({
+      exists: () => true,
+      data: () => ({
+        avatar: {
+          base: null,
+          eyes: null,
+          mouth: null,
+          accessory: null,
+        },
+      }),
+    });
+    return jest.fn(); // mock unsubscribe
+  }),
+  updateDoc: jest.fn(),
+  doc: jest.fn(),
+}));
+
+
+describe("AvatarContext unit test", () => {
+    test('initial avatar state is empty', async () => {
+        const { result } = renderHook(() => useAvatar(), { wrapper });
+
+        await waitFor(() => {
+            expect(result.current.avatar).toEqual({
+            base: null,
+            eyes: null,
+            mouth: null,
+            accessory: null,
+        })})
+    }),
+    test('updateAvatar updates correctly', async() => {
+        updateDoc.mockResolvedValue();
+        doc.mockReturnValue('mockUserRef');
+
+        const { result } = renderHook(() => useAvatar(), { wrapper });
+
+        //wait for avatar to be defined first
+        await waitFor(() => {
+            expect(result.current.avatar).toBeDefined();
+        })
+
+        await act(async () => {
+            await result.current.updateAvatar({ eyes: 'Angry_Eyes' });
+        })
+        expect(updateDoc).toHaveBeenCalledWith('mockUserRef', {
+            avatar: expect.objectContaining({ eyes: 'Angry_Eyes' })
+        })
+        expect(result.current.avatar.eyes).toBe('Angry_Eyes');
+    }), 
+    test('loads assets and sets isAssetsLoaded to true', async () => {
+        const { result } = renderHook(() => useAvatar(), { wrapper })
+
+        await waitFor(() => { 
+            expect(result.current.isAssetsLoaded).toBe(true);
+        })
+    })
+
+})
---
+++ src/jestSetup.js
@@ -0,0 +1,6 @@
+
+process.env.EXPO_OS = 'Platform.OS';
+
+const mockAsyncStorage = require('@react-native-async-storage/async-storage/jest/async-storage-mock');
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
