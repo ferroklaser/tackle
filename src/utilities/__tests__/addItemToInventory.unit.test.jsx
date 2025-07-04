c731e84ee08602ab4ff116fe92b04ca057100389
Win Paing Oo <winpaing000@gmail.com>
2025-07-04 20:29:00 +0800

    test: add test for handleItemEquip

--- src/utilities/__tests__/addItemToInventory.unit.test.jsx
+++ src/utilities/__tests__/addItemToInventory.unit.test.jsx
@@ -14,7 +14,7 @@ jest.mock('firebase/firestore', () => ({
 }))
 
 
-describe('addItemToInventory method', () => {
+describe('addItemToInventory', () => {
     beforeEach(() => {
         collection.mockClear();
         addDoc.mockClear();
