42da078a14c99b61cb8fe14895059cc3b5cdc303
jaeden <jaedenthia22@gmail.com>
2025-07-22 02:55:43 +0800

    style: loading splash animation

--- src/components/LoadingSplash.jsx
+++ src/components/LoadingSplash.jsx
@@ -1,19 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
+import { Video } from 'expo-av';
 import React from 'react'
 import { Link } from 'expo-router'
 
 const LoadingSplash = () => {
   return (
-    <View style={ styles.title }>
-      <Text>Loading...</Text>
-    </View>
+    <Video
+          source={require('../assets/videos/Loading.mp4')}
+          rate={1.0}
+          volume={1.0}
+          isMuted={true}
+          resizeMode="cover"
+          shouldPlay
+          isLooping
+          style={StyleSheet.absoluteFill}
+        />
   )
 }
 
 export default LoadingSplash
 
 const styles = StyleSheet.create({
-    title: {
+    title: { 
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
